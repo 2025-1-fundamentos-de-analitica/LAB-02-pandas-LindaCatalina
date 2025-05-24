eeee4ccf1e7afdc8af1dae6395a038b2f0b017e4
Linda Catalina Correa Lozano <licorrea@unal.edu.co>
2025-05-24 10:49:15 -0500

    P2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -6,6 +6,7 @@ librerias de pandas para resolver las preguntas.
 """
 
 
+
 def pregunta_02():
     """
     ¿Cuál es la cantidad de columnas en la tabla `tbl0.tsv`?
@@ -14,3 +15,13 @@ def pregunta_02():
     4
 
     """
+    import pandas as pd
+
+    df = pd.read_csv('./files/input/tbl0.tsv', sep='\t')
+
+    result = df.shape[1]
+
+    return(result)
+
+if __name__ == '__main__':
+    print(pregunta_02())
